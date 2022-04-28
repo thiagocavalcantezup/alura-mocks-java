c0ada39134b0f8094e43629e887bd32a3032f66f
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-28 19:17:28 -0300

    Add test for FinalizarLeilaoService using Mockito
    Changes:
    - Use constructor dependency injection
    - Edit private method to get a modifiable Lance list

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -1,8 +1,8 @@
 package br.com.alura.leilao.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.leilao.dao.LeilaoDao;
@@ -12,8 +12,11 @@ import br.com.alura.leilao.model.Leilao;
 @Service
 public class FinalizarLeilaoService {
 
-    @Autowired
-    private LeilaoDao leiloes;
+    private final LeilaoDao leiloes;
+
+    public FinalizarLeilaoService(LeilaoDao leiloes) {
+        this.leiloes = leiloes;
+    }
 
     public void finalizarLeiloesExpirados() {
         List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
@@ -26,7 +29,7 @@ public class FinalizarLeilaoService {
     }
 
     private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
-        List<Lance> lancesDoLeilao = leilao.getLances();
+        List<Lance> lancesDoLeilao = new ArrayList<>(leilao.getLances());
         lancesDoLeilao.sort((lance1, lance2) -> {
             return lance2.getValor().compareTo(lance1.getValor());
         });
---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,66 @@
+package br.com.alura.leilao.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+
+public class FinalizarLeilaoServiceTest {
+
+    private FinalizarLeilaoService finalizarLeilaoService;
+
+    @Mock
+    private LeilaoDao leilaoDao;
+
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Fulano"));
+        Lance primeiro = new Lance(new Usuario("Beltrano"), new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+
+        return lista;
+    }
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        finalizarLeilaoService = new FinalizarLeilaoService(leilaoDao);
+    }
+
+    @Test
+    void deveFinalizarUmLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        finalizarLeilaoService.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+
+        assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+        assertTrue(leilao.isFechado());
+        verify(leilaoDao).salvar(any(Leilao.class));
+    }
+
+}
